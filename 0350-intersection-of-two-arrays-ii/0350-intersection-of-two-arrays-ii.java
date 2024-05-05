f24e0665d3934f8a0ff2ce65f25e3cfca265f457
Niharik Kalpam <142443152+niharik22@users.noreply.github.com>
2024-05-04 23:07:31 -0400

    Time: 6 ms (8.49%), Space: 43.1 MB (52.89%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,26 +1,25 @@
 class Solution {
     
-    public HashMap<Integer, Integer> helper (int[] nums) {
-        HashMap<Integer, Integer> counter = new HashMap<>();
-        for(int n : nums){
-           counter.put(n,counter.getOrDefault(n,0)+1);
-        }
-        return counter;
-    } 
     public int[] intersect(int[] nums1, int[] nums2) {
         
-        ArrayList<Integer> result = new ArrayList<>(); 
-        HashMap<Integer, Integer> n1Freq = helper(nums1);
-        HashMap<Integer, Integer> n2Freq = helper(nums2);
-        for(int key : n1Freq.keySet()){
-            if(n2Freq.containsKey(key)){
-                int repeat = Math.min(n1Freq.get(key),n2Freq.get(key));
-                for(int i = 0; i<repeat;i++){
-                    result.add(key);
-                }
-            }
+      Arrays.sort(nums1);
+      Arrays.sort(nums2);
+      ArrayList<Integer> result = new ArrayList<>();
+      int i = 0;
+      int j = 0;
+      while(i< nums1.length && j<nums2.length){
+        if(nums1[i]==nums2[j]){
+            result.add(nums1[i]);
+            i++;
+            j++;
+        } else if(nums1[i]>nums2[j]){
+            j++;
+        } else if(nums1[i]<nums2[j]){
+            i++;
         }
-         int[] intersection = result.stream().filter(i -> i != null).mapToInt(i -> i).toArray();    
-        return intersection;
+      }
+     
+     int[] intersection = result.stream().filter(n->n!=null).mapToInt(n->n).toArray();
+     return intersection;
     }
 }
